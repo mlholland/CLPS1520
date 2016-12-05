9937b8b7ae7ab7a50c602e58eba1836754b56c05
Miles <miles@Miless-MacBook-Pro.local>
2016-12-04 21:31:24 -0500

    modified pull_images/sh to place images in 1 dir per class/origin file

--- code/pull_images.sh
+++ code/pull_images.sh
@@ -9,17 +9,22 @@
 #hard code txt and class_images
 txt_dir="/txt/"
 image_dir="/images/"
+all_dir="/all/"
+
 
 mkdir "$1$image_dir"
+mkdir "$1$image_dir$all_dir"
 ls "$1$txt_dir" | while read file
 do
     echo "$1$txt_dir$file"
     class=`echo "$file" | sed -E "s/(.*)\..*/\1/g"`
     counter=0
+    mkdir "$1$image_dir$class"/
     cat "$1$txt_dir$file" | while read line 
     do
-	echo "$1$image_dir$class"_"$counter".png
-	curl -0 ${line} > "$1$image_dir$class"_"$counter".png
+	echo "$1$image_dir$class"/"$class"_"$counter".png
+	curl -0 ${line} > "$1$image_dir$class"/"$class"_"$counter".png
+	ln "$1$image_dir$class"/"$class"_"$counter".png "$1$image_dir$all_dir$class"_"$counter".png 
 	counter=$((counter+1))
     done
 done
