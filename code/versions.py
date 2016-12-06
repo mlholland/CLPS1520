c4fc6fbb5f0b40cc16022682ee8e420e41d87d16
Philip Bayer <pbayer8@gmail.com>
2016-12-06 11:34:12 -0500

    fixed bug

--- code/versions.py
+++ code/versions.py
@@ -40,10 +40,12 @@ def version_control(params):
     params = version_0(params)
     params['bg_color'] = .5
     params['grid_scale'] = .9
+    return params
 
 def version_background(params):
     params = version_0(params)
     params['bg_only'] = True
+    return params
 
 #gray with two foregrounds:
 def version_1(params):
