44e08442982a6db5895f406ac00549354b7d951c
Philip Bayer <pbayer8@gmail.com>
2016-12-07 23:55:05 -0500

    plotting

--- results/plots.py
+++ results/plots.py
@@ -48,8 +48,41 @@ def combos_map():
 	plt.imshow(z, interpolation='nearest')
 	plt.show()
 
-def 
-combos_map()	
+def bg_clutter(filename):
+	bg_acc = {}
+	acc = []
+	clut = []
+	#bg_acc = {bg_filename:[# correct,# total,bg_clutter]}
+	for file in glob.iglob('accuracy_bybg*[6-9].csv'):
+		with open(file, 'rb') as f:
+			reader = csv.reader(f)
+			for i,row in enumerate(reader):
+				key = str(row[0])
+				key = key[:key.rindex('.')]
+				if key in bg_acc:
+					bg_acc[key] = [bg_acc[key][0]+float(row[2]),bg_acc[key][1]+float(row[3]),0]
+				else:
+					bg_acc[key] = [float(row[2]),float(row[3]),0]
+	with open(filename,'rb') as f:
+		reader = csv.reader(f)
+		for i,row in enumerate(reader):
+			key = str(row[0])
+			key = key[31:key.rindex('.')]
+			bg_acc[key][2] = float(row[1])
+		for k,v in bg_acc.iteritems():
+			acc.append(v[0]/v[1])
+			clut.append(v[2])
+		plt.figure()
+		plt.scatter(clut,acc)
+		plt.show()
+
+
+
+#32
+
+bg_clutter('../backgroundclutters.csv')
+bg_clutter('../backgroundcluttersmax.csv')
+#combos_map()	
 #scale_plot()
 
 
